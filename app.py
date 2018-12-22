69a1a2c3fa9092340bdeb93fa9910d2e61681762
zengla <zengla0921@gmail.com>
2018-12-22 20:39:26 +0800

    cur = conn.cursor()

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from linebot.exceptions import (
 from linebot.models import *
 import psycopg2
 conn = psycopg2.connect(database="d3l8u727fkdhuh",user="uskhmdlztebice",password="5714697bd569731729daa365947918c513374d064055ec40fd3644ed56963f0f",host="ec2-107-20-237-78.compute-1.amazonaws.com",port="5432")
+cur = conn.cursor()
 
 app = Flask(__name__)
 
