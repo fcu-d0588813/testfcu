cb22eca3cdd05db8d175e0380fb2d7231a967935
zengla <zengla0921@gmail.com>
2018-12-22 19:18:20 +0800

    1

--- app.py
+++ app.py
@@ -36,8 +36,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
 	add_data()
-	for _data in data_UserData:
-		message = TextSendMessage(text=_data.Name)
+	message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token, message)
 
 import os
