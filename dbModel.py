a56e94db54e7fbf11eaba14aa4e074bad2c59aa6
zengla <zengla0921@gmail.com>
2018-12-22 19:12:31 +0800

    初版

---
+++ dbModel.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
+app = Flask(__name__)
+app.config[
+    'SQLALCHEMY_DATABASE_URI'] = 'postgres://pjncgccqexduho:0289715df8717139a8e31f8250184006cfd201f89c1af9e0a7f4d35576eb86f5@ec2-54-235-169-191.compute-1.amazonaws.com:5432/dekuj7rc2b6nq2'
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+
+class UserData(db.Model):
+    __tablename__ = 'UserData'
+
+    Id = db.Column(db.Integer, primary_key=True)
+    Name = db.Column(db.String(64))
+    Description = db.Column(db.String(256))
+    CreateDate = db.Column(db.DateTime)
+
+    def __init__(self
+                 , Name
+                 , Description
+                 , CreateDate
+                 ):
+        self.Name = Name
+        self.Description = Description
+        self.CreateDate = CreateDate
+
+
+if __name__ == '__main__':
+    manager.run()
